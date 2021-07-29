59ce7ad2c55f27e9142ffa565b6dbb69e21d7f9a
stvgo15 <stv.gore@gmail.com>
2021-07-29 14:36:50 -0700

    second push

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,16 +7,16 @@ import './HeroSection.css'
 function HeroSection() {
     return (
         <div className='hero-container'>
-            <video src="/Videos/Video-1.mp4" autoPlay loop muted />
+            <video src="https://storage.cloud.google.com/portafoliovideo/Video-1.mp4" autoPlay loop muted />
             <h1>Welcome.</h1>
             <p>Marketing and Programming</p>
             <div className="hero-btns">
                 <Button className='btns' buttonStyle='btn--outline'
-                buttonSize='btn--large'>About Me</Button>
+                    buttonSize='btn--large'>About Me</Button>
                 <Button className='btns' buttonStyle='btn--primary'
-                buttonSize='btn--large'>Photos<i className='far fa-play-circle'/> </Button>
+                    buttonSize='btn--large'>Photos<i className='far fa-play-circle' /> </Button>
             </div>
-        
+
         </div>
     );
 }
